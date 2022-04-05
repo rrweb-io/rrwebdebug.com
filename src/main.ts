651df41b5c16c754596ccf7a3fe2cc7577efdce4
Justin Halsall <Juice10@users.noreply.github.com>
2022-04-05 14:31:25 +0200

    Refactor page load script

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,19 @@ import "./style.css";
 
 import versionsJson from "./versions.json";
 
-document.getElementById("versions")!.innerHTML = Object.entries(versionsJson)
-  .map(([version, config]) => {
-    const { rrwebVersion } = config;
-    const isDefault = "default" in config && config.default;
+function onLoad() {
+  document.getElementById("versions")!.innerHTML = Object.entries(versionsJson)
+    .map(([version, config]) => {
+      const { rrwebVersion } = config;
+      const isDefault = "default" in config && config.default;
 
-    return `<option value="${version}" ${
-      isDefault ? "selected" : ""
-    } =>${version} (rrweb v${rrwebVersion})</option>`;
-  })
-  .join("");
+      return `<option value="${version}" ${
+        isDefault ? "selected" : ""
+      } =>${version} (rrweb v${rrwebVersion})</option>`;
+    })
+    .join("");
 
-console.log('Welcome to rrwebdebug.com!')
+  console.log("Welcome to rrwebdebug.com!");
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
