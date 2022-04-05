a8877d1065c99ad7b473d939a0dfb1036599d44f
Juice10 <Juice10@users.noreply.github.com>
2022-04-05 12:31:47 +0000

    deploy: 651df41b5c16c754596ccf7a3fe2cc7577efdce4

--- assets/main.fdaaf07e.js
+++
@@ -1,1 +0,0 @@
-import{v as n}from"./versions.ad323d23.js";document.getElementById("versions").innerHTML=Object.entries(n).map(([o,e])=>{const{rrwebVersion:t}=e,r="default"in e&&e.default;return`<option value="${o}" ${r?"selected":""} =>${o} (rrweb v${t})</option>`}).join("");console.log("Welcome to rrwebdebug.com!");
